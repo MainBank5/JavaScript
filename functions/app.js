c338165d123445a69d06750a00167447b80d8659
EliudKaruga <eliudkaruga97@gmail.com>
2023-04-14 23:26:45 +0300

    first commit

---
+++ functions/app.js
@@ -0,0 +1,55 @@
+function greetings() {
+    console.log('Hello world');
+}
+
+greetings();
+
+function add (num1 , num2) {
+    console.log(num1 + num2);
+}
+
+add(90, 20);
+
+function substract (num1, num2) {
+    const results = num1 - num2;
+    return results;
+}
+
+const subResults = substract (70, 10);
+
+console.log (subResults);
+
+
+function multiplication (num1, num2) {
+    return num1 * num2;
+}
+
+const multResults = multiplication (5, 6);
+
+function user (name) {
+    return name + 'is a registered user';
+}
+
+const output = user('Eliud ');
+console.log (output);
+
+//rest parameters
+
+function test (...numbers) {
+    return numbers;
+}
+
+console.log (test(1, 5, 6, 9, 12, 20, 22));
+
+//objects as param 
+
+function loginUser (student) {
+    return `The user ${student.name} with the id of ${student.id} is logged in`;
+}
+
+const student = {
+    name: 'Karuga', 
+    id: 2,
+}
+
+console.log(loginUser(student));
