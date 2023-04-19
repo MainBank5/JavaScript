03fd270abeb853b1dca4a8aa49fe4f3f45f9fcb2
eliudkaruga <eliudkaruga97@gmail.com>
2023-04-19 13:46:52 +0300

    IIFE functions

--- functions/app.js
+++ functions/app.js
@@ -9,7 +9,7 @@ function add (num1 , num2) {
     console.log(num1 + num2);
 }
 
-add(90, 20);
+add(90, 40);
 
 function substract (num1, num2) {
     const results = num1 - num2;
@@ -93,7 +93,40 @@ const arr = (r ,s) => {
 
 console.log (arr(23, 5));
 
-//shorthand => means return so we can simplify as follows 
+//shorthand => means implicit return so we can simplify as follows 
 
 const sub = (t, v) => t - v;
 console.log(sub(100, 10));
+
+//for  a single parameter you can leave out the parenthesis ()
+
+const double = m => m * 2;
+console.log(double(40));
+
+//retuning an object 
+
+const createObj = () => ({
+    name: 'karuga', 
+    age: 26
+    
+})
+
+console.log(createObj());
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function (y){
+    console.log(y);
+})
+//function in a callback 
+numbers.forEach((n) => console.log(n));
+
+//immediately invoked functions - used to avoid global scope poullution, where two devs migh be working 
+//on the same project and may have similar variables
+
+(function () {
+    const employee = 'Levy';
+    console.log(employee);
+})();
+
+//the last parenthesis are for invoking the function()
+
