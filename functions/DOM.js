26cbb86327f6a011779dc21d19d9e750e332cf24
eliudkaruga <eliudkaruga97@gmail.com>
2023-06-06 22:24:10 +0300

    dom

---
+++ functions/DOM.js
@@ -0,0 +1,9 @@
+console.log(document.body.innerHTML);
+console.log(document.links[0]);
+console.log(document.body.innerText);
+
+console.log(document.getElementById('main'));
+const main = document.getElementById('main');
+main.innerHTML = '<h1>Hello from Main</h1>'
+
+
